367da8951b4eba348b082271bdb3c6cf8bf6e7e3
Rohit Kumar Singh <rohitt.singh15@gmail.com>
2021-11-11 12:25:42 +0530

    initial project add

---
+++ src/main/java/com/developer/mssc/beer/order/MsscBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.developer.mssc.beer.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerOrderServiceApplication.class, args);
+	}
+
+}
